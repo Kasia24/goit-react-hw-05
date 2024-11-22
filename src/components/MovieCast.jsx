c1ff3cd462889425f375674392eec7b56d8b90b5
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-22 21:38:29 +0100

    add

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "2fd9551be199200f928abc93ae4bceb1";
 
 function MovieCast() {
   const { movieId } = useParams();
