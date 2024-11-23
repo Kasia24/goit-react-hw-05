135777ef762c7418deb25c703b3f489a283004a0
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-23 15:29:34 +0100

    add

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
-function MovieList({ movies }) {
+const MovieList = ({ movies }) => {
   const location = useLocation();
 
   return (
@@ -8,12 +9,12 @@ function MovieList({ movies }) {
       {movies.map((movie) => (
         <li key={movie.id}>
           <Link to={`/movies/${movie.id}`} state={{ from: location }}>
-            {movie.title || movie.name}
+            {movie.title}
           </Link>
         </li>
       ))}
     </ul>
   );
-}
+};
 
 export default MovieList;
