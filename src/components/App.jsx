b0d01233321c10a4d80e9cf4f463f87dee1a8d08
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-23 15:28:43 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,24 @@
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation";
-import { lazy, Suspense } from "react";
 
 const HomePage = lazy(() => import("../pages/HomePage"));
 const MoviesPage = lazy(() => import("../pages/MoviesPage"));
 const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
-function App() {
-  return (
-    <>
-      <Navigation />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Navigation />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  </>
+);
 
 export default App;
