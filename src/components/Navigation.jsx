07194fbd3a7d52d9f14380fea70ca126817f25a7
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-23 17:27:31 +0100

    add

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => (
   <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/movies">Movies</NavLink>
+    <NavLink to="/">Home </NavLink>
+    <NavLink to="/movies"> Movies</NavLink>
   </nav>
 );
 
